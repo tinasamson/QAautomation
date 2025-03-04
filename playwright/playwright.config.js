674ef29c3fd2d9382526aee7b0a8b73ddaed7404
tinab.samson <tinab.samson@gmail.com>
2025-03-04 01:28:10 -0300

    add playwright file structure

---
+++ playwright/playwright.config.js
@@ -0,0 +1,23 @@
+// @ts-check
+import { chromium, defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 40 * 1000,
+  expect: {
+    timeout: 5000,
+  },
+  reporter: 'html',
+  use: {
+    browserName: 'chromium',
+  },
+});
+
