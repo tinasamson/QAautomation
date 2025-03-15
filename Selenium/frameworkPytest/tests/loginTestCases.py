ee336460163b23cdad4e4d3d880001ac37fb319a
tinab.samson <tinab.samson@gmail.com>
2025-03-15 18:01:47 -0300

    testCase1 with selenium pytest

---
+++ Selenium/frameworkPytest/tests/loginTestCases.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+class LoginPage:
+  def __init__(self, driver):
+    self.driver = driver
+
+  def test_correct_user_login(self):
+    self.driver.get("https://www.automationexercise.com/login")
+    self.driver.find_element(By.CSS_SELECTOR, '[data-qa="login-email"]').send_keys("testops@test.com")
+    self.driver.find_element(By.CSS_SELECTOR, '[data-qa="login-password"]').send_keys("password123")
+    self.driver.find_element(By.CSS_SELECTOR, '[data-qa="login-button"]').click()
+    assert self.driver.find_elements(By.CSS_SELECTOR, ".shop-menu li a")[-1].text, " Logged in as testOps"
+    assert self.driver.find_element(By.CSS_SELECTOR, "li [href='/logout']").text, " Logout"
---
+++ Selenium/frameworkPytest/tests/test_login_page.py
@@ -0,0 +1,9 @@
+from selenium import webdriver  
+from selenium.webdriver.chrome.service import Service as ChromeService  
+from webdriver_manager.chrome import ChromeDriverManager
+from tests.loginTestCases import LoginPage
+
+def test_login_functionality():
+  driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))  
+  login_page = LoginPage(driver)
+  login_page.test_correct_user_login()
