51023a09dfe125988f571c6454905dba76ec983c
Tina Samson <tinab.samson@gmail.com>
2025-03-22 17:54:18 -0300

    Add test case 2 for product page in cypress and modify readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# QAautomation
+## Welcome to the QA automation repository, here you can find the test cases used to automate
+
+### Test Case 1: Login with correct user in login page
+1. Go to the login page
+2. Enter a correct email and password
+3. Click on the login button
+4. Verify that in the menu "logged in as testOps" exist
+5. Verify that the logout button exist
+
+### Test Case 2: View all products in product page and verify product information in product detail page 
+1. Go to the product page
+2. Verify that all products are visible
+3. Validate the name and price of product exist
+4. Validate that the add to cart button exist
+5. Click on the View Product button of a random product
+6. Validate that user navegated to the product detail page
+7. Validate the product information
+
+### Test Case 3: Filter with a category the product list page
+1. Go to the product page
+2. Verify that all products are visible
+3. Verify that the category filter exist
+4. Click on a random category
+5. Verify that user navigated to that category page
+6. Verify that all products are equal or more than all products of that category
---
+++ cypress/E2E/e2e/productPage.cy.js
@@ -0,0 +1,34 @@
+describe('Product Page', () => {
+  it('Test case 2: View all products in product page and verify product information in product detail page.', () => {
+    cy.visit("https://www.automationexercise.com/products");
+    cy.get("h2.title").should("exist").should("have.text", "All Products");
+    cy.get(".product-image-wrapper").its("length").should("be.gt", 0);
+    cy.get(".product-image-wrapper").as("allProducts");
+    cy.get("@allProducts").then((allProducts)=>{
+      const numProducts = allProducts.length;
+      const randomNum = Math.floor(Math.random() * numProducts);
+      cy.wrap(allProducts).get(".productinfo h2").eq(randomNum).should("exist").invoke("text").as("productPrice");
+      cy.wrap(allProducts).get(".productinfo p").eq(randomNum).should("exist").invoke("text").as("productName");
+      cy.wrap(allProducts).get(".productinfo a").eq(randomNum).should("have.text", "Add to cart");
+      cy.wrap(allProducts).get(".choose").eq(randomNum).contains("View Product").click();
+    });
+
+    // Validate product detail information
+    cy.get("@productPrice").then((priceProductList)=>{
+      cy.get(".product-information span span").invoke("text").then((priceProductDetail)=>{
+        cy.log(priceProductDetail);
+        expect(priceProductList).to.eq(priceProductDetail)
+      });
+    });
+
+    cy.get("@productName").then((nameProductList)=>{
+      cy.get(".product-information h2").invoke("text").then((nameProductDetail)=>{
+        expect(nameProductList).to.eq(nameProductDetail)
+      });
+    });
+    cy.get(".product-information p").eq(0).contains("Category:");
+    cy.get(".product-information p").eq(1).contains("Availability");
+    cy.get(".product-information p").eq(2).contains("Condition");
+    cy.get(".product-information p").eq(3).contains("Brand");
+  })
+})
