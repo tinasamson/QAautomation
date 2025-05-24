8b9ae1854d4374b1e5d3bba6ae0e9d6bb655adb1
Tina Samson <tinab.samson@gmail.com>
2025-05-24 03:46:48 -0300

    Add testcase 3 for playwright

--- playwright/tests/productPage.spec.js
+++ playwright/tests/productPage.spec.js
@@ -7,7 +7,7 @@ test('Test case 2: View all products in PLP and verify product information in PD
   const randomIndex = Math.floor(Math.random() * totalProducts);
 
   await expect(page.locator("h2.title")).toHaveText("All Products");
-  await expect(totalProducts).toBeGreaterThan(0);
+  expect(totalProducts).toBeGreaterThan(0);
   const productPrice = await allProducts.locator(".productinfo h2").nth(randomIndex).textContent();
   const productName = await allProducts.locator(".productinfo p").nth(randomIndex).textContent();
   await expect(allProducts.locator(".productinfo a").nth(randomIndex)).toHaveText("Add to cart");
@@ -29,4 +29,27 @@ test('Test case 2: View all products in PLP and verify product information in PD
   // await expect(page.locator(".product-information p").nth(1)).toContainText("Availability");
   // await expect(page.locator(".product-information p").nth(2)).toContainText("Condition");
   // await expect(page.locator(".product-information p").last()).toContainText("Brand");
+});
+
+test('Test case 3: Filter with a category the product list page', async ({ page}) => {
+  await page.goto("https://www.automationexercise.com/products");
+  const allProducts = page.locator(".product-image-wrapper");
+  const totalProducts = await allProducts.count();
+
+  await expect(page.locator("h2.title")).toHaveText("All Products");
+  expect(totalProducts).toBeGreaterThan(0);
+  await expect(page.locator(".category-products")).not.toBeEmpty();
+  const categories = page.locator('.category-products [data-toggle="collapse"]');
+  const totalCategories = await categories.count();
+  const randomCategory = Math.floor(Math.random() * totalCategories);
+  const categoryName = await categories.nth(randomCategory).textContent();
+  await categories.nth(randomCategory).click();
+  const filters = page.locator(`#${categoryName.trim()} .panel-body ul li a`)
+  const totalFilters = await filters.count()
+  const randomFilters = Math.floor(Math.random() * totalFilters);
+  await filters.nth(randomFilters).click()
+
+  await expect(page).toHaveURL(/\/category_products\/\d/);
+  const filterProducts = await page.locator(".product-image-wrapper").count();
+  expect(totalProducts).toBeGreaterThan(filterProducts)
 });
